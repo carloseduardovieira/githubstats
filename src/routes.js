4dbc15df84caefb2bf29b2ab1f3bf8c0875e9907
Carlos Eduardo Vieira <dullvieira@gmail.com>
2020-09-14 11:02:21 -0300

    fix: general request

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,7 @@ const statsController = require('./app/controllers/stats.controller');
 
 //Routes definition
 routes.get( '/stats', statsController.getStats );
-
+routes.get('/', (req, res) => {
+  res.json({status: 'We are online'});
+})
 module.exports = routes;
